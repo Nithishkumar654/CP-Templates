f509bb8f9131bf9221e6e96fea2c0c1e84efab6e
Nithish Kumar Sirigadde <117301484+Nithishkumar654@users.noreply.github.com>
2024-06-18 22:00:48 +0530

    KMP_Algorithm.cpp

---
+++ StringAlgorithms/KMP_Algorithm.cpp
@@ -0,0 +1,35 @@
+#define ll     long long
+#define sz(x)  (ll)(x).size()
+
+void LPS(string &pattern, vector<ll>& lps){
+    int m = sz(pattern);
+    lps[0] = 0;
+    int j = 0, i = 1;
+    while(i < m){
+        while(j > 0 && pattern[j] != pattern[i]) j = lps[j - 1];
+        if(pattern[i] == pattern[j]) lps[i] = j + 1, j++;
+        else lps[i] = 0;
+        i++; 
+    }
+}
+
+vector<int> kmp(string &pattern, string &text){
+    int n = sz(text), m = sz(pattern);
+    vector<ll> lps(m + 1, 0);
+    LPS(pattern, lps);
+    int i = 0, j = 0, occurenceCount = 0;
+    vector<int> occurences;
+    while(i < n){
+        if(pattern[j] == text[i]){
+            i++; j++; 
+            if(j == m){
+                occurences.push_back(i - m);
+                occurenceCount++; j = lps[j - 1];
+            }
+            continue;
+        }
+        if(j == 0) i++;
+        else while(j > 0 && pattern[j] != text[i]) j = lps[j - 1];
+    }
+    return occurences;
+}

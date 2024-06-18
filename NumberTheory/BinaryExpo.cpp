9a100a74d78bf9bf5d7986709a4d481c460bb43a
Nithish Kumar Sirigadde <117301484+Nithishkumar654@users.noreply.github.com>
2024-06-18 22:09:46 +0530

    BinaryExpo.cpp

--- NumberTheory/BinaryExpo.cpp
+++ NumberTheory/BinaryExpo.cpp
@@ -1,10 +1,12 @@
 #define ll    long long
 #define M               1000000007
 
-ll power(int a, int b){
-    if(b == 0) return 1;
+ll power(ll a, ll b){
+    if(b == 0) return 1ll;
     ll ans = power(a, b/2);
     ans = (ans * ans)%M;
     if(b&1) return (ans * a)%M;
     return ans;
 }
+
+ll inv(ll a, ll m){ a %= m; return power(a, m - 2, m);}

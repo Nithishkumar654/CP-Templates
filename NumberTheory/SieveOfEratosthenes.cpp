f99aab1f6d0a159c4e9ab2ed78f4f10f2b8f120b
Nithish Kumar Sirigadde <117301484+Nithishkumar654@users.noreply.github.com>
2024-06-18 21:44:33 +0530

    SieveOfEratosthenes.cpp

---
+++ NumberTheory/SieveOfEratosthenes.cpp
@@ -0,0 +1,18 @@
+#define ll      long long
+
+const int N = 2e5 + 10;
+vector<bool> prime(N + 1, true);
+vector<ll> smallestPrime(N, -1);
+
+void sieve(){
+    prime[0] = prime[1] = false;
+    for(int i = 2; i * i <= N; i++){
+        if(prime[i] == true){
+            smallestPrime[i] = i;
+            for(int j = i * i; j <= N; j += i){
+                prime[j] = false;
+                if(smallestPrime[j] != -1) smallestPrime[j] = i;
+            }
+        }
+    }
+}
